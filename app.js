53f48d3447d8cebd7ea29b2c2aa1b34f52f4398b
Bhavesh Bhimte <jammerking96@gmail.com>
2022-02-06 12:28:28 +0530

    added id and js funtion

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const monthData = document.querySelector("#hours");
+// ID Data
+const hoursData = document.querySelector("#hours");
+const minutesData = document.querySelector("#minutes");
+const secondsData = document.querySelector("#seconds");
+const secondsData = document.querySelector("#months");
+const datData = document.querySelector("#day");
+const dateData = document.querySelector("#date");
+const date = new Date();
 
+// Monthname Array
 const monthName = [
   "January",
   "February",
@@ -15,6 +23,7 @@ const monthName = [
   "December",
 ];
 
+// Weekday Array
 const weekDay = [
   "Sunday",
   "Monday",
@@ -24,7 +33,7 @@ const weekDay = [
   "Friday",
   "Saturday",
 ];
-const d = new Date();
+
 
 const date = d.getDate();
 const month = monthName[d.getMonth()];
@@ -32,6 +41,8 @@ let day = weekDay[d.getDay()];
 let hours = d.getHours();
 let minutes = d.getMinutes();
 let seconds = d.getSeconds();
+
+setInterval(1000, date)
 
 monthData.innerText = month;
 
